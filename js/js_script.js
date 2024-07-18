4690bc4672f0aaf251adc5ddb416cdd21f966ff3
yeongbo2000 <yeongbo2000@gmail.com>
2024-07-18 17:36:56 +0900

    Add files via upload

---
+++ js/js_script.js
@@ -0,0 +1,26 @@
+// slide
+setInterval(function(){
+    $(".news_wrap ul").delay(2000);
+    $(".news_wrap ul").animate({
+        marginTop: '-310px'
+    },1000)
+    $(".news_wrap ul").delay(2000);
+    $(".news_wrap ul").animate({
+        marginTop: '-620px'
+    },1000)
+    $(".news_wrap ul").delay(2000);
+    $(".news_wrap ul").animate({
+        marginTop: '-930px'
+    },1000)
+    $(".news_wrap ul").delay(2000);
+    $(".news_wrap ul").animate({
+        marginTop: '-1240px'
+    },1000)
+    $(".news_wrap ul").delay(2000);
+    $(".news_wrap ul").animate({
+        marginTop: '-1550px'
+    },1000)
+    $(".news_wrap ul").animate({
+        marginTop: '0px'
+    },0)  
+});
